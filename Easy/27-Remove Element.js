f53a15c3f9fb88b73250977d731a2b06aa72d657
Gourav Manna <gouravmanna3@gmail.com>
2024-07-31 15:54:25 -0600

    fix: update the input output value

--- Easy/27-Remove Element.js
+++ Easy/27-Remove Element.js
@@ -1,5 +1,5 @@
-// Input: nums = [3,2,2,3], val = 3
-// Output: 2, nums = [2,2,_,_]
+// Input: nums = [0,1,2,2,3,0,4,2], val = 2
+// Output: 5, nums = [0,1,4,0,3,_,_,_]
 
 function removeElement(nums, val) {
   let left = 0;
