cc571c84dbd839e3d2e458337ed1784f86d5f344
Gourav Manna <gouravmanna3@gmail.com>
2024-08-17 00:40:17 -0600

    350 - Intersection of Two Arrays II

---
+++ Easy/Array/350-Intersection of Two Arrays II.js
@@ -0,0 +1,34 @@
+/*
+  Given two integer arrays nums1 and nums2, return an array of their intersection. 
+  Each element in the result must appear as many times as it shows in both arrays and 
+  you may return the result in any order.
+
+  Example 1:
+  Input: nums1 = [1,2,2,1], nums2 = [2,2]
+  Output: [2,2]
+
+  Example 2:
+
+  Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+  Output: [4,9]
+*/
+
+var intersect = function (nums1, nums2) {
+  const count = {};
+  const result = [];
+
+  for (const num of nums1) {
+    count[num] = (count[num] || 0) + 1;
+  }
+
+  for (const num of nums2) {
+    if (count[num] > 0) {
+      result.push(num);
+      count[num] = count[num] - 1;
+    }
+  }
+
+  return result;
+};
+
+console.log(intersect([1, 2, 2, 1], [2, 2]));
