f8eabfa0e1074c6670be45608b47519c3673c31d
Gourav Manna <gouravmanna3@gmail.com>
2024-09-03 19:16:54 -0600

    2. Add Numbers

---
+++ Medium/LinkedList/2. Add Numbers.js
@@ -0,0 +1,40 @@
+/*
+
+  You are given two non-empty linked lists representing two non-negative integers. 
+  The digits are stored in reverse order, and each of their nodes contains a single digit. 
+  Add the two numbers and return the sum as a linked list.
+  You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+  Input: l1 = [2,4,3], l2 = [5,6,4]
+  Output: [7,0,8]
+  Explanation: 342 + 465 = 807.
+
+*/
+
+class ListNode {
+  constructor(val, next = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+var addTwoNumbers = function (l1, l2) {
+  let dummy = new ListNode(0);
+  let current = dummy;
+
+  while (l1 !== null || l2 !== null || carry !== 0) {
+    let val1 = l1 ? l1.val : 0;
+    let val2 = l2 ? l2.val : 0;
+
+    let sum = val1 + val2 + carry;
+    carry = Math.floor(sum / 10);
+
+    current.next = new ListNode(sum % 10);
+    current = current.next;
+
+    if (l1) l1 = l1.next;
+    if (l2) l2 = l2.next;
+  }
+
+  return dummy.next;
+};
